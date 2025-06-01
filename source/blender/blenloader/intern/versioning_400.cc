fa677f7d4c86be15c89836bb52cebc228024b692
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2025-06-01 09:39:47 -0500

    Update versioning_400.cc

--- source/blender/blenloader/intern/versioning_400.cc
+++ source/blender/blenloader/intern/versioning_400.cc
@@ -453,7 +453,7 @@ void do_versions_after_linking_400(FileData *fd, Main *bmain)
       LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
         LISTBASE_FOREACH (SpaceLink *, sl, &area->spacedata) {
           if (sl->spacetype == SPACE_VIEW3D) {
-            View3D *v3d = static_cast<View3D *>(area->spacedata.first);
+            View3D *v3d = reinterpret_cast<View3D *>(sl);
             v3d->flag2 |= V3D_SHOW_CAMERA_GUIDES;
           }
         }
