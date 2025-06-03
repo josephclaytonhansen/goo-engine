d2e4bc895dd9c52b8ad38de164289b8228537ea8
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2025-06-03 09:02:21 -0500

    Update rna_scene.cc

--- source/blender/makesrna/intern/rna_scene.cc
+++ source/blender/makesrna/intern/rna_scene.cc
@@ -2399,6 +2399,10 @@ static void rna_SceneCamera_update(Main * /*bmain*/, Scene * /*scene*/, PointerR
 
   if (camera && (camera->type == OB_CAMERA)) {
     DEG_id_tag_update(&camera->id, ID_RECALC_GEOMETRY);
+    if (cam->resolution_x > 0 && cam->resolution_y > 0) {
+      scene->r.xsch = cam->resolution_x;
+      scene->r.ysch = cam->resolution_y;
+    }
   }
 }
 
