81a1995da012479c9ef8a6415b5c749cc7592fb0
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2025-06-05 15:17:09 -0500

    Update versioning_400.cc

--- source/blender/blenloader/intern/versioning_400.cc
+++ source/blender/blenloader/intern/versioning_400.cc
@@ -303,29 +303,29 @@ static void version_principled_bsdf_update_animdata(ID *owner_id, bNodeTree *ntr
   }
 }
 
-static void versioning_eevee_shadow_settings(Object *object)
-{
-  /** EEVEE no longer uses the Material::blend_shadow property.
-   * Instead, it uses Object::visibility_flag for disabling shadow casting
-   */
-
-  short *material_len = BKE_object_material_len_p(object);
-  if (!material_len) {
-    return;
-  }
-
-  using namespace blender;
-  bool hide_shadows = *material_len > 0;
-  for (int i : IndexRange(*material_len)) {
-    Material *material = BKE_object_material_get(object, i + 1);
-    if (!material || material->blend_shadow != MA_BS_NONE) {
-      hide_shadows = false;
-    }
-  }
-
-  /* Enable the hide_shadow flag only if there's not any shadow casting material. */
-  SET_FLAG_FROM_TEST(object->visibility_flag, hide_shadows, OB_HIDE_SHADOW);
-}
+//static void versioning_eevee_shadow_settings(Object *object)
+//{
+//  /** EEVEE no longer uses the Material::blend_shadow property.
+//   * Instead, it uses Object::visibility_flag for disabling shadow casting
+//   */
+//
+//  short *material_len = BKE_object_material_len_p(object);
+//  if (!material_len) {
+//    return;
+//  }
+//
+//  using namespace blender;
+//  bool hide_shadows = *material_len > 0;
+//  for (int i : IndexRange(*material_len)) {
+//    Material *material = BKE_object_material_get(object, i + 1);
+//    if (!material || material->blend_shadow != MA_BS_NONE) {
+//      hide_shadows = false;
+//    }
+//  }
+//
+//  /* Enable the hide_shadow flag only if there's not any shadow casting material. */
+//  SET_FLAG_FROM_TEST(object->visibility_flag, hide_shadows, OB_HIDE_SHADOW);
+//}
 
 static void versioning_replace_splitviewer(bNodeTree *ntree)
 {
@@ -467,37 +467,6 @@ void do_versions_after_linking_400(FileData *fd, Main *bmain)
     /* These matrices are runtime data and in 4.2 they are not contained in DNA. For those files
      * from future versions, initialize the matrices to the identity so they are valid before a
      * depsgraph update. */
-    LISTBASE_FOREACH (Object *, object, &bmain->objects) {
-      unit_m4(object->object_to_world);
-      unit_m4(object->world_to_object);
-    }
-  }
-
-  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 402, 9)) {
-    const float default_snap_angle_increment = DEG2RADF(15.0f);
-    const float default_snap_angle_increment_precision = DEG2RADF(5.0f);
-    LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
-      scene->toolsettings->snap_angle_increment_2d = default_snap_angle_increment;
-      scene->toolsettings->snap_angle_increment_3d = default_snap_angle_increment;
-      scene->toolsettings->snap_angle_increment_2d_precision =
-          default_snap_angle_increment_precision;
-      scene->toolsettings->snap_angle_increment_3d_precision =
-          default_snap_angle_increment_precision;
-    }
-  }
-
-    if (!MAIN_VERSION_FILE_ATLEAST(bmain, 402, 9)) {
-    const float default_snap_angle_increment = DEG2RADF(15.0f);
-    const float default_snap_angle_increment_precision = DEG2RADF(5.0f);
-    LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
-      scene->toolsettings->snap_angle_increment_2d = default_snap_angle_increment;
-      scene->toolsettings->snap_angle_increment_3d = default_snap_angle_increment;
-      scene->toolsettings->snap_angle_increment_2d_precision =
-          default_snap_angle_increment_precision;
-      scene->toolsettings->snap_angle_increment_3d_precision =
-          default_snap_angle_increment_precision;
-    }
-  }
 
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 402, 49)) {
     LISTBASE_FOREACH (bScreen *, screen, &bmain->screens) {
